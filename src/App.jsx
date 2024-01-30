61d52adc0bd1bf286c6f9f0603b64e2fd377aed1
LDRushing <rushinglucy@yahoo.com>
2024-01-30 17:37:35 -0600

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 function App() {
   const [count, setCount] = useState(0)
